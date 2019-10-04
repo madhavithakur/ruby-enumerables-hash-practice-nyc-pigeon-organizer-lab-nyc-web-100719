c0575ea3f8f9f4640cb6c8265090ec2e8e6d20d9
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-04 23:51:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  pigeon_list = {}
+  
+  data.each do |p_key, p_val|
+    p_val.each do |p_key1, p_val1|
+      p_val1.each do |name|
+      #if !pigeon_list.include?(name)
+         pigeon_list[name] = 
+          { 
+            color: [name][:color],
+            gender: [name][:gender],
+            lives: [name][:white]
+          }
+        end 
+       #binding.pry
+    end 
+  end 
+end
+  pigeon_list
 end
